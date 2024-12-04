f9c6704d4128f6d47c5d6782374aaed3163b9250
ataAy1 <moon_1905_1907@hotmail.com>
2024-12-04 14:46:28 +0300

    first homework

---
+++ app/src/main/java/com/hmw/hmw1/Variables.kt
@@ -0,0 +1,59 @@
+package com.hmw.hmw1
+
+
+fun main() {
+    val ilce="Bakırköy"
+    val kita="Avrupa"
+    val faks="021456214"
+    val postaKodu="34111"
+    val instaAdresi="xyz"
+    val calistiginBolum="Mobil Geliştirme"
+    val urunMiktari="50 Adet"
+    val musteriSoyadi="Aydın"
+    val odemeMiktari=424
+    val dogumTarihi="15.02.1979"
+    val borc= 5124
+    val medeniHal="Bekar"
+    val videoYorumu="Güzel bir video"
+    val odemeSaati="14:34"
+    val eftMiktari=234.0
+    val satilanMiktar=73
+    val telefonModeli="Samsung S20"
+    val dergiAdi="Uzayda bulunan maddeler"
+    val basimTarihi="14.06.2024"
+    val zamMiktari=450.0
+    val daireSayisi=40
+    val enlem=39.422
+    val boylam=24.414
+    val yemekAdi="Pirinç Pilavı"
+    val urunFiyati=34.99
+    val sirket="Amazon"
+    val videoAdi="Eğitim Videosu"
+    val muzikSuresi="4:13"
+    val mekanPuani=4.6
+    val dosyaAdi="uzay.pdf"
+    val resimFormati="JPEG"
+    val renk="Siyah"
+    val renkKodu="#0000DD"
+    val bilgisayarModeli="Macbook"
+    val ekranBoyutu=15.6
+    val kullanımSuresi="2 yıl"
+    val basinc= 1012.21
+    val etkinlikGunu="13.05.2024"
+    val odemeGunu="25.12.2024"
+    val yolculukCikisTarihi="5.12.2025"
+    val mahalleAdi="Barış"
+    val otobusHatti="34C"
+    val kulalanilanDakika=186
+    val kargoTakipNo="AD52A654"
+    val kuponSuresi="1 gün"
+    val kuponKodu="MUTLUYILLAR"
+    val faturaTarihi="11.03.2024"
+
+    println("İlçe: $ilce")
+    println("Kıta: $kita")
+    println("Telefon Modeli: $telefonModeli")
+    println("Doğum Tarihi: $dogumTarihi")
+    println("Mekan Puanı: $mekanPuani")
+
+}
